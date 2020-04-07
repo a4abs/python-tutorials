83de0abf2d58d65851f903b85544f4426f92e51b
Ankur Sharma <ankur.sharma@technocube.in>
2020-04-07 21:46:22 +0530

    Added question 2

---
+++ question-two.py
@@ -0,0 +1,5 @@
+# Get weight in pounds and convert it into kilograms
+weight_in_pounds = input("Enter weight(pounds): ")
+weight_in_kilogram = int(weight_in_pounds) * 0.45
+
+print("Your weight(kg): " + str(weight_in_kilogram))
