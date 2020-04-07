84726596f612e04e0ae373853ac5cf8bb9e28361
Ankur Sharma <ankur.sharma@technocube.in>
2020-04-07 21:33:53 +0530

    Initial commit with question1

---
+++ question-one.py
@@ -0,0 +1,6 @@
+# Get User name and favourite color and print it back!
+name = input("Please enter your name: ")
+color = input("What is your favourite color: ")
+
+print(name + " likes " + color)
+
